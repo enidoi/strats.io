61acfb13c04ea7737bc90f2da285d432e7ca3491
Chris Hughes <gotrewt@gmail.com>
2016-01-16 21:18:10 -0800

    added some CSS and borders to visualize

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ var App = React.createClass({
   getInitialState : function(){
     return {
       heroes : require('./herodata'),
-      selectedOppponents : {}
+      selectedOppponents : []
     }
   },
   opponentState : function(key) {
